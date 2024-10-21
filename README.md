7a250e6c69a598f202eb01c4892f1f722ffe78e0
zocom-johan-kivi <johan.kivi@zocom.se>
2024-10-21 08:58:31 +0200

    init Week 1

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
 # Week 1 - Hello Typescript
+
 Week 1 codealongs, excersies and workshop.
+
+## Lesson example
+
+- [Presentation Teori Vecka 1](https://docs.google.com/presentation/d/196_YOu_yrTxDcgs9t7szT6vj-2qPI0fs4Y3I0zWEbCk/edit?usp=sharing)
+- [Lesson code](./lesson-code/)
+- [Card Game](./card-game/)
+
+## Exercises
+
+**1.** Create a TypeScript program that defines variables of the following types with example values: `string`, `number`, `boolean`, and `any`. Print each variable to the console.
+
+**2.** Create a _Type_ of `Product` containing the following keys: `id`, `name`, `description` and `price`.
+
+**3.** Create a _Type_ of `Profile` containing the following keys: `firstName`, `lastName`, `email` and `tel`.
+
+**4.** Define an _array_ of numbers and calculate the sum of its elements. Use TypeScript’s array type syntax.
+
+**5.** In the `Profile` type, add a `address` key, containing a `Address` type containing the following keys: `street`, `postNr`, `city`.
+
+**6.** In the `Profile` type, add a `role` key, wich can only allow following roles: `guest`, `user`, `admin` and `superadmin`.
+
+**7.** Define an _array_ that allows only `Product` type.
+
+**8.** Create a _Type_ of `Book` containing the following keys: `title`, `author`, `publishedYear`, `publisher`, `numOfPage`. The `author` key should be a `Author` type containing the following keys: `firstName`, `lastName`.
+
+**9.** Define an _array_ that allows both `Product` and `Book` types.
+
+**10.** Write a small TS-program that has a `button` and a `h1`. When you click the `button`, a random number between 1-100 should appear in the `h1`. All varaibles etc must be typed. Top it of with a nice CSS.
+
+## Workshop project
+
+```
+TO BE ANNOUNCED
+Ooooh, how exciting!
+```
---
+++ card-game/js/script.js
@@ -0,0 +1,225 @@
+
+let activeCard,
+    previousCard,
+    attempts = 3,
+    score = 0,
+    deck = createDeck()
+
+// Init
+let picked = pickCard();
+setCard(picked);
+
+document.querySelector('#lower').addEventListener('click', lower);
+document.querySelector('#higher').addEventListener('click', higher);
+document.querySelector('#equal').addEventListener('click', equal);
+
+
+function createDeck() {
+
+    let deck = [],
+        suites = ['&spades;', '&hearts;', '&clubs;', '&diams;'];
+
+    for(let i = 0; i < suites.length; i++){
+
+        for(let j = 2; j < 15; j++){
+
+            let card = {
+                suite: suites[i],
+                color: getColor(suites[i]),
+                display: getDisplay(j),
+                value: j
+            };
+
+            deck.push(card);
+
+        }
+    }
+
+    function getColor(suite){
+        if(suite == '&hearts;' || suite == '&diams;'){
+            return 'red';
+        } else {
+            return 'black';
+        }
+    }
+
+    function getDisplay(val){
+        if (val < 11) { return val;  }
+        if (val == 11) { return 'J';  }
+        if (val == 12) { return 'D';  }
+        if (val == 13) { return 'K';  }
+        if (val == 14) { return 'A';  }
+    }
+    
+    return deck;
+}
+
+
+function pickCard(){
+
+    let rand = Math.floor(Math.random()*deck.length);
+    let pickedCard = deck.splice(rand, 1);
+
+    // Set active card for comparison
+    activeCard = pickedCard[0];
+
+    return pickedCard[0];
+}
+
+
+function setCard(card){
+
+    let el = document.createElement('article');
+    el.classList.add('card');
+
+    // Random degree for coolness
+    let deg = Math.floor(Math.random()*8);
+    el.style.transform = `rotateZ(${deg}deg)`;
+
+    el.innerHTML = 
+    `<section class="front">
+            <header><span class="${card.color}">${card.suite}</span> ${card.display}</header>
+            <div class="suite ${card.color}">${card.suite}</div>
+            <footer><span class="${card.color}">${card.suite}</span> ${card.display}</footer>        
+    </section>
+    <section class="back"></section>`;
+
+    document.querySelector('.placeholder').insertAdjacentElement('beforeend', el);
+
+}
+
+
+function updateCount(){
+    document.querySelector('.left').innerHTML = `${deck.length+1} kort kvar`
+}
+
+
+function lower(e){
+
+    if(deck.length > 0 && attempts > 1 ){
+
+        // Shift cards
+        previousCard = activeCard;
+
+        // Pick a new
+        let picked = pickCard();
+        setCard(picked);
+
+        // Evaluate picked card
+        if (activeCard.value < previousCard.value ) {
+            // if true
+            updateScore(10);
+
+        } else {
+            // if false
+            updateAttempts();
+       
+        }
+
+        // Update card count ui
+        updateCount();
+
+    } else {
+
+        let picked = pickCard();
+        setCard(picked);
+
+        setTimeout(() => {
+            document.querySelector('#gameover').classList.toggle('show');
+        }, 600)
+    }
+}
+
+
+function higher(e){
+
+    if(deck.length > 0 && attempts > 1 ){
+
+        // Shift cards
+        previousCard = activeCard;
+
+        // Pick a new
+        let picked = pickCard();
+        setCard(picked);
+
+        // Evaluate picked card
+        if (activeCard.value > previousCard.value ) {
+            // if true
+            updateScore(10);
+
+        } else {
+            // if false
+            updateAttempts();
+       
+        }
+
+        // Update card count ui
+        updateCount();
+
+    } else {
+
+        let picked = pickCard();
+        setCard(picked);
+
+        setTimeout(() => {
+            document.querySelector('#gameover').classList.toggle('show');
+        }, 600)
+
+    }
+}
+
+
+function equal(e){
+
+    if(deck.length > 0 && attempts > 1 ){
+
+        // Shift cards
+        previousCard = activeCard;
+
+        // Pick a new
+        let picked = pickCard();
+        setCard(picked);
+
+        // Evaluate picked card
+        if (activeCard.value == previousCard.value ) {
+            // if true
+            updateScore(100);
+
+        } else {
+            // if false
+            updateAttempts();
+        }
+
+        // Update card count ui
+        updateCount();
+
+    } else {
+
+        let picked = pickCard();
+        setCard(picked);
+
+        setTimeout(() => {
+            document.querySelector('#gameover').classList.toggle('show');
+        }, 600)
+    }
+}
+
+
+function updateAttempts(){
+    // Timer for cardanimation to complete
+    setTimeout(() => {
+        attempts--;
+        document.querySelector('.attempts').innerHTML = attempts;
+    }, 600);
+}
+
+
+function updateScore(factor = 10){
+
+    // Timer for cardanimation to complete
+    setTimeout(() => {
+    // Score count algorithm can be refactored to include risk taking    
+        score += previousCard.value * factor;
+        document.querySelector('.score').innerHTML = score;
+    }, 600);
+}
